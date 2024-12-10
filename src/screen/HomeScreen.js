5cdad867ea1a2a77293ada01cf867533c4d44445
Vysakh-mec <Vysakh-mec@users.noreply.github.com>
2024-12-10 21:52:10 +0530

    Removing Unnecessary components and adding some final touches

--- src/screen/HomeScreen.js
+++ src/screen/HomeScreen.js
@@ -1,9 +1,8 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import { StyleSheet } from 'react-native'
+import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import HeaderHomeScreen from '../components/HeaderHomeScreen'
 import StudentList from "../components/StudentList"
-import ClassesList from '../constants/ClassesList'
 
 const HomeScreen = () => {
 
